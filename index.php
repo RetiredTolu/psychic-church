280945a06d414a2e33db2ded3668cb343a90fcf7
Greatman Blessed <retoluf@gmail.com>
2022-10-06 12:52:50 +0100

    Learning PHP

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <html>
+    <head>
+        <meta charset="UTF-8"> 
+        <title>The Glorious Church</title>
+    </head>
+    <body>
+        <?php
+        $name = "Fly Bruh";
+
+        echo "Hello ", "World", "My name is {$name}";
+        ?>
     <div>Hello</div>
+    </body>
 </html>
